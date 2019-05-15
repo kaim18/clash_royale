97b4327c59bc6a022a0c5381091891d62887ad96
Kai Munekuni <kai.munekuni@gmail.com>
2019-05-15 14:44:14 +0900

    ugoku

--- lib/clash_royale/client.rb
+++ lib/clash_royale/client.rb
@@ -5,7 +5,7 @@ require 'uri'
 
 module ClashRoyale
   class Client
-    attr_accessor :acces_token
+    attr_accessor :access_token
     BASE_URL = 'https://api.clashroyale.com/v1'.freeze
 
     def initialize(options = {})
@@ -14,8 +14,26 @@ module ClashRoyale
       end
       yield(self) if block_given?
     end
+    def http_request(path)
+      url = URI.parse(BASE_URL + path)
+      req = Net::HTTP::Get.new(url.path)
+      req['authorization'] = "Bearer #{access_token}"
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      res = http.request(req)
+
+      return res
+    end
 
 
-
+    def clans
+      res = http_request('/clans')
+      res.body
+    end
+    def player(tag)
+      res = http_request('/players/' + tag)
+      res.body
+    end
   end
 end
